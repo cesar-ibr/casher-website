7e8eda827a2410dd79fa4057a2a509d1ebb323b3
Cesar Sanchez <cesar.sanchezibr@gmail.com>
2021-08-18 01:32:33 +0200

    Adding services and gardening

--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
 import '../assets/style.css';
 
-document.querySelector('#app').innerHTML = `<h1>Hello World!</h1>`;
+// document.querySelector('#app').innerHTML = `<h1>Hello World!</h1>`;
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  future: {},
+  purge: ['./index.html'],
+  theme: {
+      colors: {
+          body: '#F7F7F7',
+          gray: {
+              DEFAULT: '#2D3043',
+              dark: '#222433'
+          },
+          blue: {
+              DEFAULT: '#044EF1',
+              dark: '#000624'
+          },
+          orange: {
+              light: '#E2975C',
+              DEFAULT: '#F95F59'
+          },
+          white: '#FFFFFF',
+          yellow: '#FBC12D',
+          purple: '#38178F',
+          pink: '#D1316C',
+          green: '#519F20',
+      },
+  },
+  variants: {},
+  plugins: [],
+}
