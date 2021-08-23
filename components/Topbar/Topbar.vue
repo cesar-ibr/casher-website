2b37315f6f479d1609b08875594b409287fe9754
Cesar Sanchez <cesar.sanchezibr@gmail.com>
2021-08-23 20:07:28 +0200

    Adding footer

---
+++ components/Topbar/Topbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <section
+    class="
+        p-4
+        bg-orange
+        text-white
+        sticky
+        top-0
+        flex
+        items-center
+        justify-end
+        z-10
+      "
+  >
+    <a href="/" class="mr-auto">
+      <img
+        src="~/assets/img/casher-logo.png"
+        alt="Casher Logo"
+        width="50"
+      >
+    </a>
+    <a href="/#services" class="mr-8">Rubros</a>
+    <a href="/contacto">Contacto</a>
+  </section>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,37 +1,25 @@
 <template>
   <div>
-    <!-- TODO: Create Main Navbar -->
-    <section
-      class="
-        p-4
-        bg-orange
-        text-white
-        sticky
-        top-0
-        flex
-        items-center
-        justify-end
-        z-10
-      "
-    >
-      <a href="/" class="mr-auto">
-        <img
-          src="~/assets/img/casher-logo.png"
-          alt="Casher Logo"
-          width="50"
-        >
-      </a>
-      <a href="/#services" class="mr-8">Rubros</a>
-      <a href="/contacto">Contacto</a>
-    </section>
+    <topbar />
     <div class="container mx-auto">
       <Nuxt />
+    </div>
+    <!-- Footer -->
+    <div class="pt-16 pb-2 text-center">
+      <p class="text-xs">
+        Corporativo Casher &#169; 2021 | Developed by
+        <a href="https://cesarsan.dev" class="underline">cesarsan.dev</a>
+      </p>
     </div>
   </div>
 </template>
 <script>
+import Topbar from '@/components/Topbar/Topbar.vue';
+
 export default {
   name: 'DefaultLayout',
-  components: {},
+  components: {
+    Topbar,
+  },
 };
 </script>
