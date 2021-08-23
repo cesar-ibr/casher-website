d15b0529a5410ee3bb2f0b7748163d453d565a3f
Cesar Sanchez <cesar.sanchezibr@gmail.com>
2021-08-23 19:01:19 +0200

    Adding icon component

---
+++ components/Icon/Icon.vue
@@ -0,0 +1,32 @@
+<template>
+  <img
+    :src="imgPath"
+    :alt="alt"
+    :width="width"
+    class="inline"
+  >
+</template>
+<script>
+export default {
+  props: {
+    width: {
+      type: Number,
+      default: 24,
+    },
+    iconName: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    alt: {
+      type: String,
+      default: 'Icon Image',
+    },
+  },
+  computed: {
+    imgPath () {
+      return require(`~/assets/icons/${this.iconName}.svg`);
+    },
+  },
+};
+</script>
